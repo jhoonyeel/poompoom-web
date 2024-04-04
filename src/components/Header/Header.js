354de80b073f8c4f3a15213c644ef2b98e46ada8
dekoms <dekoms@naver.com>
2024-04-05 04:16:21 +0900

    style: reset css 설정

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div>header입니다.</div>
+    </div>
+  );
+}
+
+export default Header;
