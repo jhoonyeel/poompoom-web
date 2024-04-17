949867d81992b1d20abe1f24ba86953cfec8ae40
dekoms <dekoms@naver.com>
2024-04-17 16:15:20 +0900

    feat: 예시 사진 추가

--- src/components/ReviewPostBackground/ReviewPostBackground.js
+++ src/components/ReviewPostBackground/ReviewPostBackground.js
@@ -2,10 +2,7 @@ function ReviewPostBackground({ post, onPostClick, onMouseEnter }) {
   return (
     <div>
       <button type="button" onClick={() => onPostClick(post)} onMouseEnter={onMouseEnter}>
-        <img
-          src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kakaocorp.com%2Fpage%2Fservice%2Fservice%2FKakaoTalkGift&psig=AOvVaw2gPrMdRaAyubxjz5FvMBlU&ust=1713371882204000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDnrJWWx4UDFQAAAAAdAAAAABAD"
-          alt="Post 사진"
-        />
+        <img src="http://via.placeholder.com/150x150.png" alt="Post 사진" />
       </button>
     </div>
   );
