aff1e333eac80cd5a7da918aaac617daeb265631
dekoms <dekoms@naver.com>
2024-06-04 01:20:10 +0900

    feat: carco 추가

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+// craco.config.js
+module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      // Webpack 설정 변경 예시
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        publicPath: '/poompoom-web/',
+      };
+      return webpackConfig;
+    },
+  },
+  // 추가 설정 가능
+};
