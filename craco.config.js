38b00ba5c53d1c98a48554d39a855eba59e1b280
dekoms <dekoms@naver.com>
2024-06-04 04:22:32 +0900

    fix: reset.css는 적용되도록 다시 수정

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,3 @@
-// craco.config.js
 const { whenProd } = require('@craco/craco');
 
 module.exports = {
@@ -7,9 +6,10 @@ module.exports = {
       webpackConfig.output = {
         ...webpackConfig.output,
         publicPath: process.env.PUBLIC_URL || '/',
-        filename: whenProd(() => 'static/js/[name].[contenthash:8].js', 'static/js/bundle.js'),
+        filename: whenProd(() => 'static/js/[name].[contenthash:8].js', 'static/js/[name].js'),
         chunkFilename: whenProd(() => 'static/js/[name].[contenthash:8].chunk.js', 'static/js/[name].chunk.js'),
       };
+      webpackConfig.cache = false; // 캐시 비활성화
       return webpackConfig;
     },
   },
