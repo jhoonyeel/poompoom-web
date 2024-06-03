ca16036f3b3145005cc1a06c5318a1e04b1c4286
dekoms <dekoms@naver.com>
2024-06-04 03:54:15 +0900

    feat: craco.config파일에 동적으로 생성되는 script 해시파일 경로설정

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,16 @@
 // craco.config.js
+const { whenProd } = require('@craco/craco');
+
 module.exports = {
   webpack: {
-    configure: (webpackConfig, { env, paths }) => {
-      // Webpack 설정 변경 예시
+    configure: (webpackConfig) => {
       webpackConfig.output = {
         ...webpackConfig.output,
         publicPath: process.env.PUBLIC_URL || '/',
+        filename: whenProd(() => 'static/js/[name].[contenthash:8].js', 'static/js/bundle.js'),
+        chunkFilename: whenProd(() => 'static/js/[name].[contenthash:8].chunk.js', 'static/js/[name].chunk.js'),
       };
       return webpackConfig;
     },
   },
-  // 추가 설정 가능
 };
