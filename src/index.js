6cfa5c810cb35130f15cf6f2c06f010a74683121
dekoms <dekoms@naver.com>
2024-06-05 00:28:45 +0900

    fix: craco.config 수정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import axios from 'axios';
 import App from './App';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.withCredentials = true; // refreshToken cookie를 주고받기 위함
 
-const basename = process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL : '';
+const basename = process.env.NODE_ENV === 'production' ? '/poompoom-web' : '';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/Review/ReviewPage.js
+++ src/pages/Review/ReviewPage.js
@@ -1,28 +1,61 @@
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { debounce } from 'lodash';
+import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import RankingProfileCard from './components/Ranking/RankingProfileCard/RankingProfileCard.container';
-import LatestHeader from './components/Section/LatestSection/LatestHeader/LatestHeader.container';
-import LatestGallery from './components/Section/LatestSection/LatestGallery/LatestGallery.container';
-import SubHeader from './components/Section/SubSection/SubHeader/SubHeader.container';
-import SubGallery from './components/Section/SubSection/SubGallery/SubGallery.container';
-import SearchHeader from './components/Section/SearchSection/SearchHeader/SearchHeader.container';
-import SearchGallery from './components/Section/SearchSection/SearchGallery/SearchGallery.container';
 import PostFilter from './components/PostFilter/PostFilter.container';
+import RankingProfileCard from './components/Ranking/RankingProfileCard/RankingProfileCard.container';
+// import LatestGallery from './components/Section/LatestSection/LatestGallery/LatestGallery.container';
+// import LatestHeader from './components/Section/LatestSection/LatestHeader/LatestHeader.container';
+// import SearchGallery from './components/Section/SearchSection/SearchGallery/SearchGallery.container';
+// import SearchHeader from './components/Section/SearchSection/SearchHeader/SearchHeader.container';
+// import SubGallery from './components/Section/SubSection/SubGallery/SubGallery.container';
+// import SubHeader from './components/Section/SubSection/SubHeader/SubHeader.container';
 
 export default function ReviewPage() {
+  const [isSticky, setIsSticky] = useState(false);
+  const postFilterRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      const postFilter = postFilterRef.current;
+      if (postFilter) {
+        const { offsetTop } = postFilter;
+        setIsSticky(window.scrollY >= offsetTop - (12 * window.innerHeight) / 100);
+      }
+    }, 150); // 150~200ms의 디바운스 적용
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      handleScroll.cancel(); // 컴포넌트가 언마운트 될 때 디바운스 취소
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isSticky]);
+
+  // 화면 맨 위로 이동하는 함수
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // 부드러운 스크롤
+    });
+  };
+
   return (
     <>
       <RankingProfileCard />
       <Layout>
-        <PostFilter />
-        <LatestHeader />
+        <PostFilterWrapper ref={postFilterRef} isSticky={isSticky}>
+          <PostFilter />
+        </PostFilterWrapper>
+        {/* <LatestHeader />
         <LatestGallery />
         <SubHeader />
         <SubGallery />
         <SearchHeader />
-        <SearchGallery />
-        {/* <QuickGift /> */}
-        <ButtonBox>
-          <button type="button">↑</button> {/** 추후에 컴포넌트 사용 */}
+        <SearchGallery /> */}
+        <Box />
+        <ButtonBox onClick={scrollToTop}>
+          <UpIcon icon={faChevronUp} />
         </ButtonBox>
       </Layout>
     </>
@@ -34,10 +67,30 @@ const Layout = styled.div`
   margin: 0 auto;
 `;
 
+const PostFilterWrapper = styled.div`
+  position: ${({ isSticky }) => (isSticky ? 'sticky' : 'relative')};
+  top: ${({ isSticky }) => (isSticky ? '12vh' : 'auto')};
+  z-index: 6; /* 헤더와 함께 보이도록 z-index 조정 */
+  background-color: white; /* 배경색 지정하여 다른 콘텐츠와 구분 */
+  transition: top 0.3s ease; /* 부드러운 이동을 위한 transition 속성 추가 */
+`;
+const Box = styled.div`
+  height: 200vh;
+`;
+
 const ButtonBox = styled.div`
   position: fixed;
-  bottom: 20px; /* 화면 하단과의 간격 조정 */
+  bottom: 50px; /* 화면 하단과의 간격 조정 */
   right: 50px; /* 화면 우측과의 간격 조정 */
   z-index: 999; /* 다른 요소 위에 표시되도록 z-index 조정 */
-  width: 5%;
+  width: 2%;
+  border: 3px solid gray;
+  border-radius: 50%;
+  aspect-ratio: 1 / 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const UpIcon = styled(FontAwesomeIcon)`
+  font-size: 24px;
 `;
