d8cca3ac87d05fbe0ab4413474766157951714d3
dekoms <dekoms@naver.com>
2024-09-23 14:16:44 +0900

    hotfix: 폴더 삭제 후 다시 생성

---
+++ src/pages/Welcome/Welcome.js
@@ -0,0 +1,42 @@
+import { useNavigate } from 'react-router-dom';
+import { ReactComponent as Calender } from '../../assets/Login/Calender.svg';
+import { ReactComponent as Location } from '../../assets/Login/Location.svg';
+import { ReactComponent as Present } from '../../assets/Login/Present.svg';
+import { ReactComponent as Title } from '../../assets/Login/Welcome.svg';
+import * as S from './Welcomes.styles';
+
+export default function Welcome() {
+  const navigate = useNavigate();
+  const onClickToLogin = () => {
+    navigate('/login');
+  };
+
+  return (
+    <S.Container>
+      <Title />
+      <S.TextWrapper>
+        <S.Text>품품과 함께 해주셔서 감사합니다.</S.Text>
+        <S.Text>어떤 기능이 있는지 둘러볼까요?</S.Text>
+      </S.TextWrapper>
+      <S.SvgContainer>
+        <S.SvgWrapper>
+          <S.SvgText>선물추천기능</S.SvgText>
+          <Present />
+        </S.SvgWrapper>
+        <S.SvgWrapper>
+          <S.SvgText>캘린더 기능</S.SvgText>
+          <Calender />
+        </S.SvgWrapper>
+        <S.SvgWrapper>
+          <S.SvgText>데이트 코스 추천</S.SvgText>
+          <Location />/
+        </S.SvgWrapper>
+      </S.SvgContainer>
+
+      <S.TextWrapper>
+        <S.Text>생년월일, 기념일 등을 기입해주셔야 활성화됩니다.</S.Text> <S.Text>바로 하러 가볼까요?</S.Text>
+      </S.TextWrapper>
+      <S.Button onClick={onClickToLogin}>고고싱</S.Button>
+    </S.Container>
+  );
+}
