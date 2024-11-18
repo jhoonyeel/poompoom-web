d5bf784f70a453798df953595be0718592e923a1
yun yeo joung <da.young.yun13@gmail.com>
2024-11-18 21:10:17 +0900

    bug: merge오류 수정

--- src/components/Header/Header.presenter.js
+++ src/components/Header/Header.presenter.js
@@ -5,27 +5,19 @@ import * as S from './Header.styles';
 
 export default function HeaderUI({ showSearchBar, navigatePath }) {
   return (
-    <S.HeaderWrapper className="sticky">
-      <S.HeaderContent>
-        <ReissueButton />
-        <S.LogoBox onClick={handleOnClick('/')}>
-          <S.LogoSvg />
-        </S.LogoBox>
-        {showSearchBar && <SearchBar />}
-        <S.IconBox>
-          <AuthorCircleBox onClick={handleOnClick('/lovers-profile')}>
-            <WhiteCircleIcon icon={faCircle} />
-            <AuthorImgBox>
-              <AuthorImg src={LoverProfileEx} alt="프로필 사진" />
-            </AuthorImgBox>
-          </AuthorCircleBox>
-          <S.MenuBar>
-            <S.MenuItem onClick={handleOnClick('/review')}>MOOD VIEW</S.MenuItem>
-            <S.MenuItem onClick={handleOnClick('/lovers-profile')}>MY LOVER</S.MenuItem>
-            <S.MenuItem onClick={handleOnClick('/profile')}>OPTIOIN</S.MenuItem>
-          </S.MenuBar>
-        </S.IconBox>
-      </S.HeaderContent>
-    </S.HeaderWrapper>
+    <S.Wrapper className="sticky">
+      <S.LogoIcon onClick={navigatePath('/')} />
+      {showSearchBar && <SearchBar />}
+      <S.RightBox>
+        <S.LoverImgBox onClick={navigatePath('/lovers-profile')}>
+          <S.LoverImg src={LoverProfileEx} alt="프로필 이미지" />
+        </S.LoverImgBox>
+        <S.MenuBar>
+          <S.MenuItem onClick={navigatePath('/review')}>MOOD VIEW</S.MenuItem>
+          <S.MenuItem onClick={navigatePath('/lovers-profile')}>MY LOVER</S.MenuItem>
+          <S.MenuItem onClick={navigatePath('/profile')}>OPTIOIN</S.MenuItem>
+        </S.MenuBar>
+      </S.RightBox>
+    </S.Wrapper>
   );
 }
