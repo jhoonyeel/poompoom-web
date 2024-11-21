d1b1d702614d55c51b407f4dbbc0d7f3c80ee924
dekoms <dekoms@naver.com>
2024-11-21 22:13:05 +0900

    fix: endpoint 수정
    /poompoom-web에서 /로 수정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const basename = process.env.NODE_ENV === 'production' ? '/poompoom-web' : '';
+// const basename = process.env.NODE_ENV === 'production' ? '/poompoom-web' : '';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={basename}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
 );
